64b93b2aee4b8aabcb0b3ec2ac7adf5cf0906be0
tinabazhenova <tinabazhenov@gmail.com>
2023-03-27 20:46:45 -0700

    Finish reading http method

--- http/src/request.c
+++ http/src/request.c
@@ -12,15 +12,28 @@ int read_request(int fd, struct state_object * so, struct http_request * req) {
         return READ_REQUEST_EOF;
     }
 
+    char ch = ' ';
     if (strcmp(method_str, "GET ") == 0) {
-        // printf("Received GET\n");
+        req->method = HTTP_METHOD_GET;
     } else if (strcmp(method_str, "POST") == 0) {
-        // printf("Received POST\n");
+        req->method = HTTP_METHOD_POST;
+        read_fully_result = read_fully(fd, &ch, 1);
     } else if (strcmp(method_str, "HEAD") == 0) {
-        // printf("Received HEAD\n");
+        req->method = HTTP_METHOD_HEAD;
+        read_fully_result = read_fully(fd, &ch, 1);
     } else {
         // unsupported method
         // TODO: set response error code, make sure to respond later
+        return READ_REQUEST_ERROR;
+    }
+
+    if (read_fully_result == READ_FULLY_FAILURE) {
+        return READ_REQUEST_ERROR;
+    } else if (read_fully_result == READ_FULLY_EOF) {
+        return READ_REQUEST_EOF;
+    }
+    if (ch != ' ') {
+        // Invalid request: method must be followed by a space
         return READ_REQUEST_ERROR;
     }
 
